0af1d4cecb7bc76640be44ea8df2fc3380baae5e
Lodi2244 <elodieletellier1@gmail.com>
2019-02-17 22:31:51 +0100

    replaced jointable migration by model ItemOrder

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: item_orders
+#
+#  id         :bigint(8)        not null, primary key
+#  item_id    :bigint(8)
+#  order_id   :bigint(8)
+#  quantity   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class ItemOrder < ApplicationRecord
 end
