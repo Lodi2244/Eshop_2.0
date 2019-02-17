30be288cb1e244dc783eee6fdebee608582c071f
Lodi2244 <elodieletellier1@gmail.com>
2019-02-17 20:01:28 +0100

    created table and controller Orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: orders
+#
+#  id          :bigint(8)        not null, primary key
+#  total_price :float
+#  status      :string
+#  user_id     :bigint(8)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Order < ApplicationRecord
   belongs_to :users
 end
