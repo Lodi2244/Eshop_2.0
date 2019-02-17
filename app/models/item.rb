eb1f90b440b1c1e9c1db6af3c841e512398f906e
Lodi2244 <elodieletellier1@gmail.com>
2019-02-17 23:19:18 +0100

    added method add_to_cart

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,4 +22,12 @@ class Item < ApplicationRecord
   validates :price, presence: true, numericality: { only_float: true }
 
   has_many_attached :item_images
+
+  def add_to_cart
+    item = Item.find(item_id)
+    current_cart = current_user.carts.items
+    current_cart << item
+    flash[:success] = 'Item successfully added to cart!'
+    redirect_to item_path(params[:id])
+  end
 end
