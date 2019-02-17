2fa8b8f129374763b983be50600777154feb6869
Lodi2244 <elodieletellier1@gmail.com>
2019-02-17 20:29:18 +0100

    migrated table cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: carts
+#
+#  id         :bigint(8)        not null, primary key
+#  quantity   :integer
+#  user_id    :bigint(8)
+#  item_id    :bigint(8)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Cart < ApplicationRecord
   belongs_to :user
   belongs_to :item
