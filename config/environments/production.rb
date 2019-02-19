0bbeb138120aa7d9e49281e191a700922fe1474b
Lodi2244 <elodieletellier1@gmail.com>
2019-02-19 15:26:15 +0100

    modified config mailer production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 25 }
+  config.action_mailer.default_url_options = { host: 'maildev', port: 25 }
 
 end
