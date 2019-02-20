3de6775cc1a14b299c899482d0847bf83e85fb10
Lodi2244 <elodieletellier1@gmail.com>
2019-02-20 09:59:34 +0100

    moved method set_current_cart in item controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,5 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  before_action :set_current_cart
   def index; end
-
-  private
-
-  def set_current_cart
-    @cart = current_cart
-  end
 end
