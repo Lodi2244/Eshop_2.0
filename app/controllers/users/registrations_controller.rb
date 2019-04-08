61663b5d938f2277f7d38a7c80fdfeb45ce3fb75
Lodi2244 <elodieletellier1@gmail.com>
2019-04-08 17:08:50 +0200

    added pics

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,39 +2,40 @@
 
 module Users
   class RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+    before_action :configure_sign_up_params, only: [:create]
+    # before_action :configure_account_update_params, only: [:update]
 
-  def new
-    super
-  end
+    def new
+      super
+    end
 
-  def create
-    super
-  end
+    def create
+      super
+    end
 
-  def edit
-    super
-  end
+    def edit
+      super
+    end
 
-  def update
-    super
-  end
+    def update
+      super
+    end
 
-  def destroy
-    super
-  end
+    def destroy
+      super
+    end
 
-protected
+    private
 
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username email password password_confirmation])
+    def configure_sign_up_params
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[username email password password_confirmation])
+    end
   end
 end
 # If you have extra params to permit, append them to the sanitizer.
- # def configure_account_update_params
- #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
- # end
+# def configure_account_update_params
+#   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+# end
 
 # The path used after sign up.
 # def after_sign_up_path_for(resource)
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,26 @@
 # frozen_string_literal: true
+
 module Users
   class SessionsController < Devise::SessionsController
-  before_action :configure_sign_in_params, only: [:create]
+    before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  def new
-    super
-  end
+    def new
+      super
+    end
 
-  # POST /resource/sign_in
-  def create
-    super
-  end
+    def create
+      super
+    end
 
-  # DELETE /resource/sign_out
-  def destroy
-    super
-  end
+    def destroy
+      super
+    end
 
-  protected
+    private
 
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: %i[login password password_confirmation])
-  end
+    # If you have extra params to permit, append them to the sanitizer.
+    def configure_sign_in_params
+      devise_parameter_sanitizer.permit(:sign_in, keys: %i[login password password_confirmation])
+    end
+ end
 end
