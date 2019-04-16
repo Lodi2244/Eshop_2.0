8227eeaee3d2cf49954fdb9c845fdcddeeb78aae
Crysicia <lucienmontmartre@yahoo.fr>
2019-04-16 12:48:04 +0200

    Fix devise sign-in & Add alert/notice on layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,11 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :configure_devise_parameter, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def configure_devise_parameter
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
