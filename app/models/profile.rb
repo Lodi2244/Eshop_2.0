4632481cb3e0fafe29adc68576b683a21a3f1fba
Crysicia <lucienmontmartre@yahoo.fr>
2019-04-22 13:33:16 +0200

    Add simple validations to profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -14,5 +14,9 @@
 #
 
 class Profile < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+
   belongs_to :user
 end
