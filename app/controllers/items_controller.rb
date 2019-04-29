93dc8518e6095a44403974ab1047d1f7afb2bdc9
Lodi2244 <elodieletellier1@gmail.com>
2019-04-29 17:26:51 +0200

    [wip]integrated template for cart shows

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,7 @@ class ItemsController < ApplicationController
     respond_to do |format|
       if @item.update(item_params)
         format.html { redirect_to @item, notice: 'Item was successfully updated.' }
+        format.js
         format.json { render :show, status: :ok, location: @item }
       else
         format.html { render :edit }
@@ -33,6 +34,7 @@ class ItemsController < ApplicationController
     respond_to do |format|
       if @item.save
         format.html { redirect_to @item, notice: 'Item was successfully created.' }
+        format.js
         format.json { render :show, status: :created, location: @item }
       else
         format.html { render :new }
